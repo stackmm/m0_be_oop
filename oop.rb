a0e4e3fb0f47ce1d7b1aa6e9f26cdf25d700c4b4
Steven Stackhouse <stackmm@gmail.com>
2023-02-23 16:30:58 -0700

    add code and self-eval

--- oop.rb
+++ oop.rb
@@ -2,7 +2,17 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn
+    def initialize(name)
+        @name = name
+        @color = "silver"
+    end
 
+    def say(message)
+        return "*~* #{message} *~*"
+    end
+
+end
 
 
 #  Write a class called Vampire
@@ -10,6 +20,24 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+    def initialize(name, pet, thirsty)
+        @name = name
+
+        if pet != "bat"
+            @pet = pet
+        else
+            @pet = "bat"
+        end
+
+        @thirsty = true
+    end
+
+    def drink
+        @thirsty = false
+    end
+
+end
 
 
 #  Write a Dragon class
@@ -18,6 +46,23 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+    end
+
+    def eat(num)
+        if num >= 4
+            @is_hungry = false
+        end
+    end
+
+end
+
 
 
 #  Write a Hobbit class
@@ -28,3 +73,35 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit(name, disposition)
+    def initialize
+        @name = name
+        @disposition = disposition
+        @age = 0
+
+        if @age >= 33
+            @is_adult = true
+        else
+            @is_adult = false
+        end
+        
+        if @age >= 101
+            @is_old = true
+        else
+            @is_old = false
+        end
+
+        if @name == "Frodo"
+            @has_ring = true
+        else
+            @has_ring = false
+        end
+
+    end
+
+    def celebrate_birthday
+        @age += 1
+    end
+
+end
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,10 @@
 # Self-Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+I felt pretty good about the challenge. There are a few parts that probably could have been done more efficiently in Ruby, but it might just be outside of what I currently know. I am pretty sure the way I wrote everything would work in the way that was intended. I think I have a pretty good understanding of object-oriented programming, but this is where the formatting and methods really start changing quite a bit from what I am used to in Java.
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+I have been able to maintain my focus so far. I just take a 10-15 min break after each section, along with a lunch/dinner break, and I seem to be doing alright.
+
 - What did you learn from this exercise that you want to remember as you begin Mod 1?
+I think just the formatting of classes in Ruby is something I will have to practice many times before I can remember it quickly. The lack of semicolons at the end of each line has been really easy to screw up.
